b23fc582c671d6f30c9c81822a40627d2b937148
touat amina <touatamina98@gmail.com>
2021-02-12 18:12:44 +0100

    first commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+apiurl ="http://localhost:5000/tasks";
+  constructor(private http: HttpClient) { 
+ 
+  }
+  findAll(){
+   return this.http.get<Task[]>(this.apiurl);
+  }
+  delete(id){
+    return this.http.delete(`${this.apiurl}/${id}`);
+  }
+  persist(task){
+    return this.http.post<Task>(this.apiurl, task);
+  }
+  completed(id, completed){
+    return this.http.patch(`${this.apiurl}/${id}`, {completed: !completed});
+  }
+update(task){
+return this.http.put(`${this.apiurl}/${task.id}`, task);
+}
+
+}
